babd28681f680f0b7d4658ab95a53e84497632b2
altf4arnold <arnold.dech@adct.be>
2020-03-29 10:36:41 +0200

    Error checking user config

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ This file contains the configs for the Program
 
 # This is the name of the site. Either MRS or PAR
 location = "PAR"
+
+# defines if you want the program to compare to your IRR
+asusage = True
+
+# AS Number (must be a INT)
+asn = 35395
--- main.py
+++ main.py
@@ -18,13 +18,36 @@ def jsonparsser(location):
 		("https://www.franceix.net/api/members/list/json?location={}"\
         .format(location)))
     except:
-        toreturn = 'Error'
+        print("Error - request timeout !")
+        toreturn = {}
     return toreturn
 
-
+def confchecker():
+    """
+    here to tell if the user did put garbage in his config file
+    """
+    no_error = True
+    if config.location != "PAR" and config.location != "MRS":
+        print(config.location + " in config is garbage")
+        no_error = False
+    if type(config.asusage) != bool:
+        print("asusage in config must be a boolean")
+        no_error = False
+    if type(config.asn) != int:
+        print(str(config.asn) + " in config is garbage")
+        no_error = False
+    if config.asn > 4294967295 or config.asn < 1:
+        print(str(config.asn) + " is not a valid ASN")
+        no_error = False
+    if bool(config.asn > 64495 and config.asn < 131072) or bool(config.asn > 399260 and config.asn <= 4294967295):
+        print(str(config.asn) + " is a private ASN")
+        no_error = False
+    return no_error
 
 
 def main():
+    if confchecker() is False:
+        return 0
     print(jsonparsser(config.location))
 
 
