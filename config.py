7bc7912075585f09aa0e7f81a69d9a85d07d8e9f
altf4arnold <arnold.dech@adct.be>
2020-03-29 09:38:58 +0200

    adding the JSON polling system

---
+++ config.py
@@ -0,0 +1,6 @@
+"""
+This file contains the configs for the Program
+"""
+
+# This is the name of the site. Either MRS or PAR
+location = "PAR"
--- main.py
+++ main.py
@@ -5,4 +5,28 @@ MAR/2020 during confinement
 
 import os
 import json
+import config
 from urllib import request
+
+
+def jsonparsser(location):
+    """
+    go get the data, make the first parssing and return the data
+    """
+    try:
+        toreturn = json.load(request.urlopen\
+		("https://www.franceix.net/api/members/list/json?location={}"\
+        .format(location)))
+    except:
+        toreturn = 'Error'
+    return toreturn
+
+
+
+
+def main():
+    print(jsonparsser(config.location))
+
+
+if __name__ == '__main__':
+	main()
